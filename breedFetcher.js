a91b629d768ded3a2f191f61b4c3926fff274ef3
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2022-11-24 17:03:17 -0500

    refactored to use function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,24 @@
 const request = require('request');
 
-const breedType = process.argv.slice(2);
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedType}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    process.exit;
-
-  }
-  const data = JSON.parse(body);
-
-  if (data[0] === undefined) {
-    console.log("Breed not found");
-    process.exit;
-   
-  }
-  console.log(data[0].description);
-
-});
 
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    if (data[0] === undefined) {
+      callback("Breed not found", null);
+      return;
+    }
+
+    callback(null, data[0].description);
+
+  });
+
+};
+module.exports = { fetchBreedDescription };
